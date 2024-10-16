91f70bd996ddf131cbf5e8760dae85587c45fe60
josephshichung <154393551+josephshichung@users.noreply.github.com>
2024-10-16 16:09:41 -0400

    Update YearlyGraphWidget.dart

--- lib/YearlyGraphWidget.dart
+++ lib/YearlyGraphWidget.dart
@@ -1,1 +1,21 @@
 
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class YearlyGraphWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barGroups: [
+          BarChartGroupData(x: 0, barRods: [
+            BarChartRodData(toY: 500, color: Colors.blue),
+          ]),
+          BarChartGroupData(x: 1, barRods: [
+            BarChartRodData(toY: 400, color: Colors.red),
+          ]),
+        ],
+      ),
+    );
+  }
+}
