554827a3a8da6e23f0151aaa8b47878f007f7e66
Benjamin Lucas <ben_lucas11@aol.com>
2024-11-02 15:16:27 -0400

    reworked transaction table

--- lib/widgets/transactionswidget.dart
+++ lib/widgets/transactionswidget.dart
@@ -13,6 +13,7 @@ class _TransactionWidgetState extends State<TransactionWidget> {
   List<Transaction> currentTransactions = [];   // list of transaction objects
   List<List<String>> currentTransactionStrings = [];  // list of transaction objects as strings for display
   List<bool?> columnSorts = List.filled(Transaction().getProperties().keys.length, null);   // fill null for however many columns we have
+  Map<int, TableColumnWidth> columnSizes = {};
 
   // load transactions on startup
   @override
@@ -25,6 +26,21 @@ class _TransactionWidgetState extends State<TransactionWidget> {
     currentTransactions.add(Transaction(id:0, dates:'2010-10-16', cardn:999, content:'purchase', category: '', cost:12.00));
     currentTransactions.add(Transaction(id:1, dates:'2010-10-12', cardn:200, content:'fun', category: '', cost:120.00));
     currentTransactions.add(Transaction(id:2, dates:'2010-11-13', cardn:999, content:'going out', category: '', cost:2.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'this is a really long description', category: 'and a category', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
+    currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
     currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
     currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
     currentTransactions.add(Transaction(id:3, dates:'2011-10-14', cardn:999, content:'food', category: '', cost:1000.00));
@@ -44,10 +60,8 @@ class _TransactionWidgetState extends State<TransactionWidget> {
     setState(() {});
   }
 
-  DataTable createDataTable() {
-    List<DataColumn> myColumns = [];
-    List<DataRow> myRows = [];
-
+  List<Container> createDataTableHeaders() {
+    List<Container> myHeaders = [];
     // create default transaction for display
     if (currentTransactions.isEmpty) {
       currentTransactions.add(Transaction());
@@ -58,40 +72,83 @@ class _TransactionWidgetState extends State<TransactionWidget> {
     int cindex = 0;
     props.forEach((header, value) {
       // loop through the transaction to get the columns
-      myColumns.add(
-        DataColumn(
-          label: Row(
+      double cwidth = 150;
+      BoxDecoration decoration;
+      Color headerColor = Colors.blueAccent;
+      if(header.length < 3) {
+        cwidth = 80;
+      } else if (header.length < 7) {
+        cwidth = 90;
+      }
+      if (cindex == 0) {
+        decoration = BoxDecoration(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
+            color: headerColor
+        );
+      } else if(cindex == props.length-1) {
+        decoration = BoxDecoration(
+            borderRadius: BorderRadius.only(topRight: Radius.circular(10)),
+            color: headerColor
+        );
+      } else {
+        decoration = BoxDecoration(
+          color: headerColor
+        );
+      }
+      myHeaders.add(        
+        Container(
+          decoration: decoration,
+          width: cwidth,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-                Text(header),
-                getColumnIcon(cindex)   // dynamically generate icon based on sorting state
+              Text(header),
+              getColumnIcon(cindex)  // dynamically generate icon based on sorting state
             ]
           ),
-          numeric: value is int || value is double,
-          onSort: (columnIndex, direction) {
-            if (direction) {
-              sortMe(columnIndex);  // sort transactions
-            }
-          }
-        ));
+        )
+      );
+      columnSizes.addEntries([MapEntry(cindex, FixedColumnWidth(cwidth))]);
       cindex++;
     });
+    return myHeaders;
+  }
+
+  Table createDataTable() {
+    List<TableRow> myRows = [];
 
     // loop through the transactions to create the cells and rows
     int index = 0;
     for (List<String> row in currentTransactionStrings) {
-      List<DataCell> myCells = [];
+      List<TableCell> myCells = [];
       for (String cell in row) {
-        myCells.add(DataCell(Text(cell)));
+        myCells.add(
+          TableCell(
+            child: Container(
+              height: 30,
+              padding: EdgeInsets.only(left: 10),
+              alignment: Alignment.centerLeft,
+              color: index % 2 != 0 ? Color.fromARGB(255, 255, 255, 255) : Color.fromARGB(255, 201, 240, 255),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Text(cell)),
+            ),
+          )
+        );
       }
       myRows.add(
-        DataRow(
-          cells: myCells,
-          color: index % 2 != 0 ? WidgetStateProperty.all(Colors.blue.shade50) : WidgetStateProperty.all(Colors.white)
+        TableRow(
+          children: myCells,
         )
       );
       index++;
     }
-    return DataTable(columns: myColumns, rows: myRows);
+    return Table(
+        border: TableBorder.symmetric(),
+        columnWidths: columnSizes,
+        defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+        children: myRows,
+      );
   }
 
   void sortMe(int cindex) {
@@ -167,38 +224,48 @@ class _TransactionWidgetState extends State<TransactionWidget> {
     return transactionStrings;
   }
 
-  Transform getColumnIcon(int cindex) {
+  IconButton getColumnIcon(int cindex) {
+    Transform myIcon;
     if (columnSorts[cindex] == null) {
       // unsorted column
-      return Transform.rotate(
+      myIcon = Transform.rotate(
         angle: 0,
         child: const Icon(Icons.arrow_left_rounded)
       );
     } else if (columnSorts[cindex] == false) {
       // column sorted lowest to highest
-      return Transform.rotate(
+      myIcon = Transform.rotate(
         angle: 0,
         child: const Icon(Icons.arrow_drop_down_rounded)
       );
     } else {
       // column sorted highest to lowest
-      return Transform.rotate(
+      myIcon = Transform.rotate(
         angle: 180 *math.pi / 180,
         child: const Icon(Icons.arrow_drop_down_rounded)
       );
     }
+    return IconButton(onPressed: () {sortMe(cindex);}, icon: myIcon, padding: EdgeInsets.zero);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        height: 500,
-        child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: createDataTable()
-      )
-      )
-    ]);
+    return Expanded(
+      child: Column(children: [
+        Row(
+          children: createDataTableHeaders(),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          constraints: BoxConstraints(
+            maxHeight: 500
+          ),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: createDataTable()
+          ),
+        )
+      ]),
+    );
   }
 }
